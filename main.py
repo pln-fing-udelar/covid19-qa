1b9ed116abe387e048cc03664c389ccf920d6445
Santiago Gongora <sgongora@fing.edu.uy>
2020-03-28 15:32:16 -0300

    Project structure

--- main.py
+++ main.py
@@ -1,14 +1,67 @@
 #!/usr/bin/env python
 from transformers import pipeline
+from xml.dom import minidom
+from nltk.tokenize import sent_tokenize
+import os
 
+DATASET_PATH = './data'
 
-def qa(dataset):
-    """Given a dataset with multiple texts, returns the top 10 most confident paragraphs with the highlighted answer.
+def get_most_relevant_docs(dataset, question, n):
+    # Given a question and a dataset, returns the 'n' most relevant docs to find the answer
+    relevant_docs = []
     
-    Highlighted text *like this*.
-    """
+    return relevant_docs # list containing 'n' text_id's
+
+def generate_context_snippets(dataset, relevant_docs, snippet_size):
+    # A snippet is a set of snippet_size sentences.
+    context_snippets = []
+
+    for text_id in relevant_docs:
+        xml = minidom.parse(os.path.join(DATASET_PATH, text_id + ".xml"))
+        article = xml.getElementsByTagName('article')
+        article_text = article[0].firstChild.nodeValue
+        sentences = sent_tokenize(article_text)
+        offset = len(sentences)//snippet_size
+        remainder = len(sentences) % snippet_size
+
+        for x in range(offset):
+            snippet_text = ','.join(
+                map(str, sentences[x*snippet_size:(x+1)*snippet_size]))
+            context_snippets.append(snippet_text)
+
+        if remainder > 0:
+            snippet_text = ','.join(
+                map(str, sentences[offset*snippet_size:offset*snippet_size + remainder]))
+            context_snippets.append(snippet_text)
+
+    return context_snippets
+
+def highlight_answer(context,question):
+    # Given a context and a question, returns a pair (highlighted answer, score)
+
     pass
 
+def rank_answers(answers):
+    # Each element in 'answers' has a score. Returns a list sorted in descending order.
+    
+    answers.sort(reverse=True, key=lambda x: x[1])
+
+    return answers
+
+def qa(dataset, question):
+    # Given a dataset with multiple texts, returns the top 10 most confident paragraphs with the highlighted answer.
+    # Highlighted text *like this*.
+
+    number_of_docs = 10
+    snippet_size = 5
+    docs = get_most_relevant_docs(dataset,question,number_of_docs)
+    context_snippets = generate_context_snippets(dataset,docs,snippet_size)
+    for context in context_snippets:
+        answers.append(highlight_answer(context,question))
+    
+    print(rank_answers(answers))
+
+    pass
 
 def test():
     PATH_MODEL_FOLDER = "model"
@@ -29,4 +82,4 @@ def test():
 
 
 if __name__ == "__main__":
-    test()
+    qa(DATASET_PATH, "¿Cuándo fue el primer caso de COVID-19?")
