bd5bddd424a5d5bae740ae967079a5a2fd78b60d
Pablo Grill <pgrill@sophilabs.com>
2020-04-22 16:56:57 -0300

    Harcode a frequent questions example.

--- external-api/external_api/urls.py
+++ external-api/external_api/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from questions.views import AnswerFeedbackApiView, QuestionApiView
+from questions.views import AnswerFeedbackApiView, QuestionApiView, FrequentQuestionsApiView
 
 urlpatterns = [
     url('admin/', admin.site.urls),
     url('question/', QuestionApiView.as_view()),
-    url('feedback/', AnswerFeedbackApiView.as_view())
+    url('feedback/', AnswerFeedbackApiView.as_view()),
+    url('frequent-questions/', FrequentQuestionsApiView.as_view())
 ]
